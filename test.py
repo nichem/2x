654408fadb6153686a29b7597aa8f26bdfef38a9
scyw <scyw@gmail.com>
2023-11-30 18:39:17 +0800

    bug fix test时，读取参数未输出到指定设备

--- test.py
+++ test.py
@@ -7,7 +7,8 @@ model = load_model(False)
 from dataset import *
 
 input, target = testDataset[0]
-
+input = input.to(DEVICE)
+target = target.to(DEVICE)
 output = model(torch.unsqueeze(input, 0))
 
 utils.save_image(input, "out/test_input.jpg")
